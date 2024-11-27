bd5ac918fffb864588be9ffe0b33fa60452512d7
tamatona25 <tamatonastar2519@gmail.com>
2024-11-27 12:06:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,55 @@
-function convertToBinary() {
-  const input = document.getElementById('input').value;
-  const output = input
-    .split('')
-    .map(char => char.charCodeAt(0).toString(2).padStart(8, '0'))
-    .join(' ');
-  document.getElementById('output').value = output || '入力が空です';
+var BAAS = BAAS || {};
+
+BAAS.cocoa = {
+ init:function(){
+  this.setParameters();
+  this.bindEvent();
+ },
+
+ setParameters:function(){
+  this.$name = $('#jsi-name');
+  this.$textArea = $('#jsi-msg');
+  this.$board = $('#jsi-board');
+  this.$button = $('#jsi-button');
+
+  //各自登録時に出たコードに書き換え。「chatRoom」は任意でok。複数の部屋を作りたい場合はここを動的にする。
+  this.chatDataStore = new MilkCocoa('各自').dataStore('chatRoom');
+ },
+
+ bindEvent:function(){
+  var self = this;
+  this.$button.on('click',function(){ //トークを送信
+   self.sendMsg();
+  });
+
+  this.chatDataStore.on('push',function(data){ //発言者・トークを監視
+   self.addText(data.value.user);
+   self.addText(data.value.message);
+  });
+ },
+
+ //ユーザー、メッセージ送信
+ sendMsg:function(){
+  if (this.$textArea.val() == ''){ return }
+
+  var self = this;
+  var name = this.$name.val();
+  var text = this.$textArea.val();
+
+  self.chatDataStore.push({user:name, message:text},function(data){
+   self.$textArea.val('');
+  });
+ },
+
+ //受け取り後の処理
+ addText:function(json){
+   var msgDom = $('<li>');
+   msgDom.html(json);
+   this.$board.append(msgDom[0]);
+ }
 }
 
-function convertToText() {
-  const input = document.getElementById('input').value;
-  try {
-    const output = input
-      .split(' ')
-      .map(bin => String.fromCharCode(parseInt(bin, 2)))
-      .join('');
-    document.getElementById('output').value = output || '入力が空です';
-  } catch {
-    document.getElementById('output').value = 'バイナリ形式が正しくありません';
-  }
-}
+$(function(){
+ BAAS.cocoa.init();
+});
 
-function copyToClipboard() {
-  const output = document.getElementById('output');
-  output.select(); // テキストエリア全体を選択
-  output.setSelectionRange(0, 99999); // モバイル対応
-  navigator.clipboard.writeText(output.value)
-    .then(() => {
-      alert('コピーしました！');
-    })
-    .catch(() => {
-      alert('コピーに失敗しました');
-    });
-}
